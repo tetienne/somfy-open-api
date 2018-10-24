4f60d2e927224d6a86a14fb754d590e048cc9358
ETIENNE <thibaut@etienne.pw>
2018-10-24 23:10:01 +0200

    CREATE setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='SomfyOpenAPI',
+    version='0.1',
+    description='A Somfy Open API library',
+    author='ETIENNE Thibaut',
+    url='https://github.com/tetienne/somfy-open-api',
+    license='GNU General Public License v3.0',
+    install_requires=['requests-oauthlib']
+)
