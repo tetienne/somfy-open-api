111f25497c4a4d02448c5e9ad2730e34dbcbf0ed
ETIENNE <thibaut@etienne.pw>
2018-10-25 22:15:26 +0200

    FIX Coverage was not correctly compute

--- tests/test_somfy_api.py
+++ tests/test_somfy_api.py
@@ -1,11 +1,14 @@
+import os
+
 import httpretty
 
 from src.api.somfy_api import BASE_URL, SomfyApi
 
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @httpretty.activate
 def test_sites():
-    with open('get_sites.json', 'r') as get_sites:
+    with open(os.path.join(CURRENT_DIR, 'get_sites.json'), 'r') as get_sites:
         httpretty.register_uri(httpretty.GET, BASE_URL + '/site', body=get_sites.read())
     sites = SomfyApi('foo', 'faa').get_sites()
     assert len(sites) == 2
