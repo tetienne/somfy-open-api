9c8c12879e2f9313cc28629770116132e3896c9c
ETIENNE Thibaut <thibaut@etienne.pw>
2018-10-29 12:02:39 +0100

    ADD minimum python version due to requests-oauthlib

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
+import os
 from distutils.core import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='SomfyOpenAPI',
     version='0.1',
     description='A Somfy Open API library',
+    long_description=os.path.join(here, 'README.md'),
     author='ETIENNE Thibaut',
     url='https://github.com/tetienne/somfy-open-api',
     license='GNU General Public License v3.0',
+    python_requires='>=3.4',
     install_requires=['requests-oauthlib']
 )
