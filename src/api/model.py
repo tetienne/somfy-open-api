42f9e54bfe038b6f4fce1238006b130468892a71
ETIENNE Thibaut <thibaut@etienne.pw>
2018-10-31 10:04:36 +0100

    UPDATE made all the models serializable

--- src/api/model.py
+++ src/api/model.py
@@ -1,15 +1,16 @@
 from typing import Dict, Union, List
 
 
-class Site:
+class Site(dict):
     __slots__ = 'id', 'label'
 
     def __init__(self, json: Dict[str, str]):
         self.id = json.get('id')
         self.label = json.get('label')
+        dict.__init__(self, id=self.id, label=self.label)
 
 
-class Device:
+class Device(dict):
     __slots__ = 'id', 'name', 'type', 'site_id', 'states', 'capabilities', \
                 'categories'
 
@@ -21,39 +22,43 @@ class Device:
         self.categories = json.get('categories')
         self.states = [State(s) for s in json.get('states')]
         self.capabilities = [Capability(c) for c in json.get('capabilities')]
+        dict.__init__(self, id=self.id, name=self.name, type=self.type, site_id=self.site_id, categories=self.categories, states=self.states, capabilities=self.capabilities)
 
 
-class State:
+class State(dict):
     __slots__ = 'name', 'value', 'type'
 
     def __init__(self, json: Dict[str, str]):
         self.name = json.get('name')
         self.value = json.get('value')
         self.type = json.get('type')
+        dict.__init__(self, name=self.name, value=self.value, type=self.type)
 
 
-class Capability:
+class Capability(dict):
     __slots__ = 'name', 'parameters'
 
     def __init__(self, json: Dict[str, str]):
         self.name = json.get('name')
         self.parameters = [ParameterDescription(p) for p in
                            json.get('parameters')]
+        dict.__init__(self, name=self.name, parameters=self.parameters)
 
 
-class ParameterDescription:
+class ParameterDescription(dict):
     __slots__ = 'name', 'type'
 
     def __init__(self, json: Dict[str, str]):
         self.name = json.get('name')
         self.type = json.get('type')
+        dict.__init__(self, name=self.name, type=self.type)
 
 
 class Command(dict):
     __slots__ = 'name', 'parameters'
 
     def __init__(self, name: str, parameters: Union[
-            List[Dict[str, str]], Dict[str, str]] = None):
+        List[Dict[str, str]], Dict[str, str]] = None):
         if parameters is None:
             parameters = []
         if not isinstance(parameters, list):
