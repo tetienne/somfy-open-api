d1953693d4496dce89d4c8316a6884c93a82d34e
Thibaut Etienne <thibaut@etienne.pw>
2019-11-04 00:17:44 +0100

    EASE model, only command and parameter have to serializable

--- pymfy/api/model.py
+++ pymfy/api/model.py
@@ -1,16 +1,15 @@
 from typing import Dict, Union, List, Any
 
 
-class Site(dict):
+class Site:
     __slots__ = "id", "label"
 
     def __init__(self, json: Dict[str, str]):
         self.id = json.get("id")
         self.label = json.get("label")
-        dict.__init__(self, id=self.id, label=self.label)
 
 
-class Device(dict):
+class Device:
     __slots__ = "id", "name", "type", "site_id", "states", "capabilities", "categories"
 
     def __init__(self, json: Dict[str, Any]):
@@ -21,59 +20,31 @@ class Device(dict):
         self.categories = json.get("categories")
         self.states = [State(s) for s in json.get("states")]
         self.capabilities = [Capability(c) for c in json.get("capabilities")]
-        dict.__init__(
-            self,
-            id=self.id,
-            name=self.name,
-            type=self.type,
-            site_id=self.site_id,
-            categories=self.categories,
-            states=self.states,
-            capabilities=self.capabilities,
-        )
 
 
-class State(dict):
+class State:
     __slots__ = "name", "value", "type"
 
     def __init__(self, json: Dict[str, str]):
         self.name = json.get("name")
         self.value = json.get("value")
         self.type = json.get("type")
-        dict.__init__(self, name=self.name, value=self.value, type=self.type)
 
 
-class Capability(dict):
+class Capability:
     __slots__ = "name", "parameters"
 
     def __init__(self, json: Dict[str, str]):
         self.name = json.get("name")
         self.parameters = [ParameterDescription(p) for p in json.get("parameters")]
-        dict.__init__(self, name=self.name, parameters=self.parameters)
 
 
-class ParameterDescription(dict):
+class ParameterDescription:
     __slots__ = "name", "type"
 
     def __init__(self, json: Dict[str, str]):
         self.name = json.get("name")
         self.type = json.get("type")
-        dict.__init__(self, name=self.name, type=self.type)
-
-
-class Command(dict):
-    __slots__ = "name", "parameters"
-
-    def __init__(
-        self, name: str, parameters: Union[List[Dict[str, str]], Dict[str, str]] = None
-    ):
-        if parameters is None:
-            parameters = []
-        if not isinstance(parameters, list):
-            parameters = [parameters]
-        self.name = name
-        self.parameters = parameters
-        dict.__init__(self, name=name, parameters=parameters)
 
 
 class Parameter(dict):
@@ -83,3 +54,16 @@ class Parameter(dict):
         self.name = name
         self.value = value
         dict.__init__(self, name=name, value=value)
+
+
+class Command(dict):
+    __slots__ = "name", "parameters"
+
+    def __init__(self, name: str, parameters: Union[List[Parameter], Parameter] = None):
+        if parameters is None:
+            parameters = []
+        if not isinstance(parameters, list):
+            parameters = [parameters]
+        self.name = name
+        self.parameters = parameters
+        dict.__init__(self, name=name, parameters=parameters)
