6b003bfb4d02fdf86970ac0db8ddaed8bfee6575
Thibaut <thibaut@etienne.pw>
2021-05-14 22:12:46 +0200

    Fix wrong type (#120)

--- pymfy/api/devices/roller_shutter.py
+++ pymfy/api/devices/roller_shutter.py
@@ -10,7 +10,7 @@ class RollerShutter(SomfyDevice):
     def get_position(self) -> int:
         return cast(int, self.get_state("position"))
 
-    def set_position(self, value: int, low_speed: Optional[int] = False) -> None:
+    def set_position(self, value: int, low_speed: Optional[bool] = False) -> None:
         command_name = "position_low_speed" if low_speed else "position"
         command = Command(command_name, Parameter("position", value))
         self.send_command(command)
